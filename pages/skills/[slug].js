33dafc946b17a0c910fb8deaa916296e47f1350f
trambz <bertrand.coizy@gmail.com>
2020-11-12 10:34:27 +0100

    Adding dynamic content

---
+++ pages/skills/[slug].js
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+function Skill({ title, content, image, categories }) {
+	return (
+		<div className="container">
+			<div style={{ width: '50%', margin: '2em', display: 'flex' }}>
+				<div>
+					<h3>{title}</h3>
+					<p>{content}</p>
+					<div style={{ color: '#33F' }}>
+						[
+						{categories?.map(
+							(category, i) =>
+								`${category}${i < categories.length - 1 ? ' - ' : ''}`
+						)}
+						]
+					</div>
+				</div>
+				<div>
+					<Image width={250} height={250} src={`/images/${image}`} />
+				</div>
+			</div>
+			<div style={{ display: 'flex' }}>
+				<Link href="/">
+					<button>Homepage</button>
+				</Link>
+				<Link href="/skills">
+					<button>Skill List</button>
+				</Link>
+			</div>
+		</div>
+	);
+}
+
+export async function getStaticPaths() {
+	const data = await require('../../data/skills.json');
+	const paths = data.skills.map((page) => {
+		return { params: { slug: page.slug } };
+	});
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps({ params }) {
+	// Call an external API endpoint to get posts.
+	// You can use any data fetching library
+	const data = require('../../data/skills.json');
+
+	const skill = data.skills.find((skill) => skill.slug === params.slug);
+	const { title, content, image, categories } = skill;
+	// By returning { props: posts }, the Blog component
+	// will receive `posts` as a prop at build time
+	return {
+		props: {
+			title,
+			content,
+			image,
+			categories,
+		},
+	};
+}
+
+export default Skill;
--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,51 +1,21 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import Footer from '../../components/Footer';
 
-export default function Skills() {
-	const skills = [
-		{
-			title: 'Conseil & Gestion de projet',
-			content:
-				'Nous vous accompagnons dans l’expression de vos besoins, la rédaction des cahiers des charges, l’analyse de faisabilité, la définition des solutions techniques, le chiffrage de vos projets et leur mise en œuvre.',
-			image: 'Conseil_et_gestions.png',
-		},
-		{
-			title: 'Domaine Applicatif',
-			content:
-				"Nous intervenons sur tous les métiers d'études et développement, de la conception au déploiement, en passant par le pilotage MOE, le développement logiciel et la recette applicative.",
-			image: 'domaine_applicatif.png',
-		},
-		{
-			title: 'Systèmes & Réseaux',
-			content:
-				"Nous réalisons l’architecture, l’implémentation, le déploiement et le support des infrastructures de vos Systèmes d'Information.",
-			image: 'infrastructure_et_reseaux.png',
-		},
-		{
-			title: 'Cybersécurité',
-			content:
-				'Notre filiale Cogital, experte en Cybersécurité, réalise des prestations d’audit, de conseil, d’intégration de solutions en partenariat avec les éditeurs, de tests d’intrusion et vous accompagne sur la sécurité opérationnelle de vos SI.',
-			image: 'Cybersecurite.png',
-		},
-		{
-			title: 'Data',
-			content:
-				'Ensemble nous donnons du sens à vos données. De la data gouvernance à la datavisualisation, nous vous accompagnons sur la valorisation de votre patrimoine informatif.',
-			image: 'Picto_Data.png',
-		},
-	];
-
+export default function Skills({ skills }) {
 	return (
 		<div className="container">
-			<h1 className="title">Some Néo-Soft skills</h1>
+			<p className="description">Some Néo-Soft skills</p>
 			<div className="grid">
 				{skills.map((skill, i) => (
-					<div className="card">
-						<h3>{skill.title}</h3>
-						<p>{`${skill.content.substring(0, 35)}...`}</p>
-						<Image src={`/images/${skill.image}`} width={125} height={125} />
-					</div>
+					<Link key={i} href={`/skills/${skill.slug}`}>
+						<a className="card">
+							<h3>{skill.title}</h3>
+							<p>{`${skill.content.substring(0, 35)}...`}</p>
+							<Image src={`/images/${skill.image}`} width={125} height={125} />
+						</a>
+					</Link>
 				))}
 			</div>
 			<Footer />
@@ -55,6 +25,7 @@ export default function Skills() {
 						line-height: 1.5;
 						font-size: 1.5rem;
 					}
+
 					.grid {
 						display: flex;
 						align-items: center;
@@ -63,6 +34,7 @@ export default function Skills() {
 						max-width: 800px;
 						margin-top: 3rem;
 					}
+
 					.card {
 						margin: 1rem;
 						flex-basis: 45%;
@@ -74,21 +46,25 @@ export default function Skills() {
 						border-radius: 10px;
 						transition: color 0.15s ease, border-color 0.15s ease;
 					}
+
 					.card:hover,
 					.card:focus,
 					.card:active {
 						color: #0070f3;
 						border-color: #0070f3;
 					}
+
 					.card h3 {
 						margin: 0 0 1rem 0;
 						font-size: 1.5rem;
 					}
+
 					.card p {
 						margin: 0;
 						font-size: 1rem;
 						line-height: 1.5;
 					}
+
 					@media (max-width: 600px) {
 						.grid {
 							width: 100%;
@@ -99,4 +75,17 @@ export default function Skills() {
 			</style>
 		</div>
 	);
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+	const data = require('../../data/skills.json');
+
+	return {
+		props: {
+			skills: data.skills,
+		},
+	};
 }
